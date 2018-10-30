d3136fc6a4c13114e83a5aa35c6eb7654717e2bb
sr4wc <sr4wc@debian>
2018-10-30 19:11:37 +0530

    Add skeleton views and urls

---
+++ textsearch/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url, include
+
+from . import views
+
+app_name = 'textsearch'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^search/$', views.search, name='search'),
+]
