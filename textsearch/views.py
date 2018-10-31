34b882a54aabf821594bcc7bdef35fb5397cce92
sr4wc <sr4wc@debian>
2018-10-31 17:13:33 +0530

    Add url query retrieval and file handling

--- textsearch/views.py
+++ textsearch/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest, \
+                        QueryDict
+import csv
 
 # Create your views here.
 
@@ -6,4 +9,18 @@ def index(request):
     pass
 
 def search(request):
-    pass
+    if request.method == 'GET':
+        url_query_dict = QueryDict(request.META['QUERY_STRING'])
+
+        search_word = url_query_dict.get('word') ## get search argument out 
+                                                 ## of querystring 
+
+        ## Open file 
+        with open('word_search.tsv', 'r') as infile:
+            reader = csv.reader(infile, delimiter='\t')
+            for word in reader:
+                print word[0]
+        data = {'success': True, 'status': 'printing' }    
+    else:
+        return HttpResponseBadRequest()
+    return JsonResponse(data, safe=True)    
