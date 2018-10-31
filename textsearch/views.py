c18ff47902966b313182c43b5cd3f196637a9c8c
sr4wc <sr4wc@debian>
2018-11-01 02:03:37 +0530

    Add regular expressions for word matching and key value list as a type for ranked list

--- textsearch/views.py
+++ textsearch/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest, \
                         QueryDict
-import csv
+import csv, re
 
 # Create your views here.
 
@@ -11,16 +11,31 @@ def index(request):
 def search(request):
     if request.method == 'GET':
         url_query_dict = QueryDict(request.META['QUERY_STRING'])
-
         search_word = url_query_dict.get('word') ## get search argument out 
                                                  ## of querystring 
 
+        ## regular expr that does middle of a word match
+        re_midmatch = re.compile('[a-z]+'+search_word+'[a-z]+') 
+
+        ## regular expr for front of a word match
+        re_frontmatch = re.compile(search_word+'[a-z]+') 
+
+        match_list = {} ## Change matchlist from set to dict as it is more
+                        ## convinient for rankings 
+        counter = 0
         ## Open file 
         with open('word_search.tsv', 'r') as infile:
             reader = csv.reader(infile, delimiter='\t')
             for word in reader:
-                print word[0]
-        data = {'success': True, 'status': 'printing' }    
+                if search_word == word[0] or re_midmatch.match(word[0]):
+                    match_list[counter] = word[0]
+                    counter+=1
+                if counter is 25:
+                    break
+        ## replace the square brackets from a list into curly json type brackets
+        #curly_match_list = list(match_list).__str__().replace('[', '{') \
+        #                                             .replace(']', '}')
+        data = {'success': True, 'matches': match_list }    
     else:
         return HttpResponseBadRequest()
     return JsonResponse(data, safe=True)    
